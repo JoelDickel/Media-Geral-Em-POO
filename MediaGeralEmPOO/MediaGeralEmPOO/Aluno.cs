8fff4b51e63c3b6d56c4689e9e3f42e759bafef7
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2023-04-04 11:13:00 -0300

    Criando classe Aluno

---
+++ MediaGeralEmPOO/MediaGeralEmPOO/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralEmPOO
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+
+        private double[] _notas;
+
+        public double Media
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
